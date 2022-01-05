82f4e6aef5bab67874cc3cfe6220968ad8e97336
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2022-01-05 09:16:13 +0700

    Add Dependencies

---
+++ src/main/java/com/BagasiHidroponik/Bagasi/Hidroponik/BagasiHidroponikApplication.java
@@ -0,0 +1,13 @@
+package com.BagasiHidroponik.Bagasi.Hidroponik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagasiHidroponikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagasiHidroponikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BagasiHidroponik/Bagasi/Hidroponik/BagasiHidroponikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BagasiHidroponik.Bagasi.Hidroponik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagasiHidroponikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
